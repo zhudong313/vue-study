9eb327b59e690ca7f5cd79ccda1dbd613feee8df
dony <Dony.Z@paxdata.com>
2020-03-14 11:51:19 +0800

    add

--- node/server.js
+++ node/server.js
@@ -106,6 +106,64 @@ app.get('/get/news/detail',function(require,response){
   }
 });
 
-http.createServer(app).listen(8088,function(){
-  console.log('8088启动成功');
+app.get('/get/index/introduce',function(require,response){
+  //console.log(typeof require.query,require.query);
+  response.json({
+    code: 0,//错误码
+    msg: 'success',//错误消息
+    data: {
+      imgUrl: 'http://localhost:8089/upload/intr.jpg',
+      description: '大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月'
+    },
+  });
+});
+
+app.get('/get/index/products',function(require,response){
+  //console.log(typeof require.query,require.query);
+  response.json({
+    code: 0,//错误码
+    msg: 'success',//错误消息
+    data: [
+      { id: 1, title: '产品电器F0000001', createDate: 1583547371000, imgUrl: 'http://localhost:8089/upload/1.jpg'},
+      { id: 2, title: '产品电器F0000002', createDate: 1583547371000, imgUrl: 'http://localhost:8089/upload/1.jpg'},
+      { id: 3, title: '产品电器F0000002', createDate: 1583547371000, imgUrl: 'http://localhost:8089/upload/1.jpg'},
+      { id: 4, title: '产品电器F0000002', createDate: 1583547371000, imgUrl: 'http://localhost:8089/upload/1.jpg'},
+      { id: 5, title: '产品电器F0000002', createDate: 1583547371000, imgUrl: 'http://localhost:8089/upload/1.jpg'},
+    ],
+  });
+});
+
+app.get('/get/index/img/news',function(require,response){
+  //console.log(typeof require.query,require.query);
+  response.json({
+    code: 0,//错误码
+    msg: 'success',//错误消息
+    data: [
+      { id: 1, createDate: 1583547371000, imgUrl: 'http://localhost:8089/upload/1.jpg', title: 'news1'},
+      { id: 2, createDate: 1583547371000, imgUrl: 'http://localhost:8089/upload/intr.jpg', title: 'news2'},
+      { id: 3, createDate: 1583547371000, imgUrl: 'http://localhost:8089/upload/map.jpg', title: 'news3'},
+    ],
+  });
+});
+
+app.get('/get/index/news',function(require,response){
+  //console.log(typeof require.query,require.query);
+  response.json({
+    code: 0,//错误码
+    msg: 'success',//错误消息
+    data: [
+      { id: 1, createDate: 1583547371000, title: '的说法撒旦发射撒旦撒旦飒沓1'},
+      { id: 2, createDate: 1583547371000, title: '的说法撒旦发射撒旦撒旦飒沓2'},
+      { id: 3, createDate: 1583547371000, title: '的说法撒旦发射撒旦撒旦飒沓3'},
+      { id: 4, createDate: 1583547371000, title: '的说法撒旦发射撒旦撒旦飒沓4'},
+      { id: 5, createDate: 1583547371000, title: '的说法撒旦发射撒旦撒旦飒沓5'},
+      { id: 6, createDate: 1583547371000, title: '的说法撒旦发射撒旦撒旦飒沓6'},
+    ],
+  });
+});
+
+
+
+http.createServer(app).listen(8089,function(){
+  console.log('8089启动成功');
 });
--- src/views/index.vue
+++ src/views/index.vue
@@ -3,33 +3,21 @@
   <div class="main_middle">
     <div class="main_tit main_M_tit">
         <h3>公司简介<i>ABOUT US</i></h3>
-          <a href="##">更多&gt;&gt;</a>
+          <router-link to="/contact">更多&gt;&gt;</router-link>
       </div>
       <div class="main_M_intr">
-        <img src="@/assets/img/intr.jpg" width="140" height="140" alt="公司简介" />
-          <p>大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月，大余县xxxxxx公司城里于2002年9月...</p>
+        <img :src="introduceData.imgUrl" width="140" height="140" alt="公司简介" />
+          <p>{{ introduceData.description }}</p>
       </div>
       <div class="main_tit main_M_tit">
         <h3>产品简介<i>PRODUCTS</i></h3>
-          <a href="##">更多&gt;&gt;</a>
+          <router-link to="/product/list">更多&gt;&gt;</router-link>
       </div>
-      <div class="main_pro_list">
-        <div class="main_pro_div">
-              <dl>
-                  <dt><a href="##"><img src="@/assets/img/1.jpg" width="125" height="125" alt="产品1" /></a></dt>
-                  <dd><a href="##">产品电器F0000001号</a></dd>
-              </dl>
-              <dl>
-                  <dt><a href="##"><img src="@/assets/img/1.jpg" width="125" height="125" alt="产品1" /></a></dt>
-                  <dd><a href="##">产品电器F0000001号</a></dd>
-              </dl>
-              <dl>
-                  <dt><a href="##"><img src="@/assets/img/1.jpg" width="125" height="125" alt="产品1" /></a></dt>
-                  <dd><a href="##">产品电器F0000001号</a></dd>
-              </dl>
-              <dl>
-                  <dt><a href="##"><img src="@/assets/img/1.jpg" width="125" height="125" alt="产品1" /></a></dt>
-                  <dd><a href="##">产品电器F0000001号</a></dd>
+      <div class="main_pro_list" @mouseenter="productAnimateEnter" @mouseleave="productAnimateLeave">
+        <div class="main_pro_div" :style="{ 'margin-left': -productAnimate.move+'px' }">
+              <dl v-for="item in productsData" :key="item.id">
+                  <dt><router-link :to="`/product/detail/${item.id}`"><img :src="item.imgUrl" width="125" height="125" :alt="item.title" /></router-link></dt>
+                  <dd><router-link :to="'/product/detail/'+item.id">{{ item.title }}</router-link></dd>
               </dl>
            </div>
       </div>
@@ -37,39 +25,97 @@
   <div class="main_right">
     <div class="main_tit main_R_tit">
         <h3>行业资讯<i>News</i></h3>
-          <a href="##">更多&gt;&gt;</a>
+          <router-link to="/news/list">更多&gt;&gt;</router-link>
       </div>
       <div class="main_R_flash">
         <ul class="m_R_img">
-            <li><a href="##"><img width="190" height="190" src="@/assets/img/1.jpg" alt="图" /></a></li>
-              <li class="m_R_imgCur"><a href="##"><img width="190" height="190" src="@/assets/img/intr.jpg" alt="图" /></a></li>
-              <li><a href="##"><img width="190" height="190" src="@/assets/img/contact.jpg" alt="图" /></a></li>
+            <li :style="{ display: imgNewsActive == index ? '' : 'none' }" v-for="(item,index) in imgNewsData" :key="item.id"><router-link :to="`/news/detail/${item.id}`"><img width="190" height="190" :src="item.imgUrl" :alt="item.title" /></router-link></li>
           </ul>
           <ul class="m_R_btn">
-            <li>1</li>
-              <li class="m_R_btnCur">2</li>
-              <li>3</li>
+            <li :class="{ 'm_R_btnCur': index == imgNewsActive }" v-for="(item,index) in imgNewsData" :key="item.id" @mouseenter="imgNewsActive = index;">{{ index+1 }}</li>
           </ul>
       </div>
       <ul class="main_R_news">
-          <li><i>&gt;</i><a href="##">的说法撒旦发射撒旦撒旦飒沓</a></li>
-          <li><i>&gt;</i><a href="##">的说法撒旦发射撒旦撒旦飒沓</a></li>
-          <li><i>&gt;</i><a href="##">的说法撒旦发射撒旦撒旦飒沓</a></li>
-          <li><i>&gt;</i><a href="##">的说法撒旦发射撒旦撒旦飒沓</a></li>
-          <li><i>&gt;</i><a href="##">的说法撒旦发射撒旦撒旦飒沓</a></li>
-          <li><i>&gt;</i><a href="##">的说法撒旦发射撒旦撒旦飒沓</a></li>
+          <li v-for="item in newsData" :key="item.id"><i>&gt;</i><router-link :to="`/news/detail/${item.id}`">{{ item.title }}</router-link></li>
       </ul>
   </div>
 </div>
 </template>
 
 <script>
+import { getIndexIntroduce, getIndexProducts, getIndexImgNews, getIndexNews } from '@/api/index';
+
 export default {
   data () {
     return {
+      introduceData: {},//公司介绍数据
+      productsData: [],
+      imgNewsData: [],
+      newsData: [],
 
+      imgNewsActive: 0,//当前幻灯片选中位置
+
+      productAnimate: {//产品动画参数
+        move: 0,//移动的距离
+        totalMove: 148,
+        timeout: null,//记录计时器id
+      },
     }
-  }
+  },
+  created(){
+    this.getIndexIntroduce();
+    this.getIndexProducts();
+    this.getIndexImgNews();
+    this.getIndexNews();
+    this.productAnimateMove(0);
+  },
+  methods: {
+    getIndexIntroduce(){
+      getIndexIntroduce({}).then((res) =>{//箭头函数，箭头函数中this还是父作用域的this；传统函数this指向调用此函数的对象
+        var data = res.data.data;
+        this.introduceData = data;
+      });
+    },
+    getIndexProducts(){
+      getIndexProducts({}).then((res) =>{//箭头函数，箭头函数中this还是父作用域的this；传统函数this指向调用此函数的对象
+        var data = res.data.data;
+        this.productsData = data;
+      });
+    },
+    getIndexImgNews(){
+      getIndexImgNews({}).then((res) =>{//箭头函数，箭头函数中this还是父作用域的this；传统函数this指向调用此函数的对象
+        var data = res.data.data;
+        this.imgNewsData = data;
+      });
+    },
+    getIndexNews(){
+      getIndexNews({}).then((res) =>{//箭头函数，箭头函数中this还是父作用域的this；传统函数this指向调用此函数的对象
+        var data = res.data.data;
+        this.newsData = data;
+      });
+    },
+    productAnimateMove(step){//产品动画移动
+      step += this.productAnimate.totalMove/60;
+      if(step >= this.productAnimate.totalMove){//移动完成
+        this.productAnimate.move = 0;
+        let shiftData = this.productsData.shift();//删除数组的第一个元素
+        this.productsData.push(shiftData);
+        this.productAnimateMove(0);
+      }
+      else{
+        this.productAnimate.timeout = setTimeout(() =>{
+          this.productAnimate.move = step;
+          this.productAnimateMove(step);
+        },20);
+      }
+    },
+    productAnimateEnter(){//鼠标移入触发
+      clearTimeout(this.productAnimate.timeout);
+    },
+    productAnimateLeave(){//鼠标移出触发
+      this.productAnimateMove(this.productAnimate.move);
+    },
+  },
 }
 </script>
 
